44d8263502fb8ba9976841a473243acc1c301f87
Tibor Udvari <tibor.udvari@icloud.com>
2019-12-22 17:07:09 +0100

    Menu implementation
    Closes #2

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let elBurger = document.querySelector('.hamburger');
 elBurger.addEventListener('click', () => {
     elBurger.classList.toggle('is-active');
-    
+    elBurger.parentElement.classList.toggle('is-active');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// window.scrollTo({
+//     top: 100,
+//     left: 100,
+//     behavior: 'smooth'
+//   });
